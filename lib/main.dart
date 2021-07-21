39ce397f0702dcf17bf58010bccabb8af9b94d1c
준성 <madox9999@gmail.com>
2021-07-21 21:46:10 +0900

    dateTime

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_nosmoking/ui/date_picker.dart';
 import 'package:flutter_nosmoking/ui/home.dart';
-import 'package:flutter_nosmoking/ui/sum_page.dart';
+import 'package:intl/intl.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple
       ),
-      home: SumPage(),
+      title: 'Flutter DateTimePicker Demo',
+      home: Home(),
+        localizationsDelegates: [
+          GlobalWidgetsLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [Locale('ko', '')], //, Locale('pt', 'BR')],
     );
   }
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    int daysBetween(DateTime from, DateTime to) {
+      from = DateTime(from.year, from.month, from.day,from.hour);
+      to = DateTime(to.year, to.month, to.day,to.hour);
+      return (to.difference(from).inHours / 24).round();
+    }
+
+    //the birthday's date
+    final birthday = DateTime(2021, 09, 15, 04, 45);
+    final date2 = DateTime.now().add(Duration(hours: 9))
+    ;
+    final difference = daysBetween(birthday, date2);
+    var format = 'yyyy-MM-dd HH'
+        ':mm';
+    print(difference);
+    print(birthday);
+    print(date2);
+    return Container(
+    );
   }
 }
